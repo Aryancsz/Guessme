e0f935c596727bb444241f2c4268a2ebedb0bb36
aryancsz <mallikarjuncrazysoul@gmail.com>
2022-07-30 21:17:22 +0530

    remove console

--- script.js
+++ script.js
@@ -14,8 +14,6 @@ let score = 20;
 let highScore = 0;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
-
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number';
   } else if (guess === seacretNumber) {
